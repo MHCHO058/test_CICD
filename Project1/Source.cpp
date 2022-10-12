3ef2981449d7514933cd37537f93a90daea9dd4f
MHCHO058 <34257964+MHCHO058@users.noreply.github.com>
2022-10-12 10:45:55 +0900

    Add files via upload

---
+++ Project1/Source.cpp
@@ -0,0 +1,7 @@
+int solution(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	return 0;
+}
---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(4, solution(1, 3));
+			Assert::AreEqual(4, solution(2,2));
+
+		}
+
+		TEST_METHOD(TestMethod2) {
+			Assert::AreEqual(0, solution(1, 4));
+		}
+	};
+}
